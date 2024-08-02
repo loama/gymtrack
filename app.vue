883b5427988498d1944e9fc25759e03dd2c1db38
Eduardo López <loama18@gmail.com>
2024-08-02 23:47:05 +0200

    add link to results

--- app.vue
+++ app.vue
@@ -2,6 +2,13 @@
   <div
     class="title fixed top-0 left-0 w-full h-12 text-center leading-[50px] bg-gray-dark border-b border-zinc-800 z-10">
     GYMTRACK
+
+    <NuxtLink
+      to="/results"
+      class="absolute right-2 top-0 border border-zinc-500 leading-8 top-2 text-sm h-8 rounded-md px-2 bg-black text-zinc-500"
+      v-if="$route.path !== '/results'"
+      >See Results
+    </NuxtLink>
   </div>
   <div class="pt-12">
     <NuxtPage />
